4c8d6c23e1c6f3d00e84bc6368c35ec72e7c6db7
Sümer Cip <sumer.cip@platform.sh>
2024-03-05 17:16:44 +0300

    fix periodMillis

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ function defaultLabels() {
 }
 
 /** time in milliseconds for which to send the collected profile. */
-global.periodMillis = 1_000; // TODO: 45_000
+global.periodMillis = 45_000;
 
 const defaultConfig = {
   /** name of the application */
