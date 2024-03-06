3dc029e3394f6ff3cce248811d2a250b799c082d
Sümer Cip <sumer.cip@platform.sh>
2024-03-06 12:21:44 +0300

    tidy examples

--- examples/example3.js
+++ examples/example3.js
@@ -1,19 +1,5 @@
 const Blackfire = require('@blackfireio/node-tracing');
 
-// process.env.DD_PROFILING_UPLOAD_PERIOD = 1;
-// process.env.DD_PROFILING_UPLOAD_TIMEOUT = 1000;
-// process.env.DD_INSTRUMENTATION_TELEMETRY_ENABLED = "False";
-// process.env.DD_PROFILING_EXPERIMENTAL_CPU_ENABLED = 1;
-// process.env.DD_TRACE_AGENT_URL = "unix:///opt/homebrew/var/run/blackfire-agent.sock";
-
-// const tracer = require('dd-trace').init({
-//     profiling: true,
-//     env: 'prod',
-//     service: 'my-app1',
-//     version: '1.0.3',
-//     tags: { 'build.id': 'my-build-id' },
-// })
-
 Blackfire.start({labels: {"application_name": "my-example3-nodejs"}});
 
 const sleep = (milliseconds) => {
